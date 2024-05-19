bb38c0a6e40cea1b9afcafeb25760e4c50742e57
Antonious-Awad <antonuostony1@gmail.com>
2024-05-19 23:54:31 +0300

    chore: modifying base styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,21 @@
+import { colors as systemColors } from './src/styles/colors'
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,ts,tsx}"],
+  content: ['./src/**/*.{html,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: systemColors,
+      fontSize: {
+        h1SemiBold: '2rem',
+      },
+      fontFamily: {
+        sans: ['Poppins', 'ui-sans-serif'],
+      },
+    },
   },
   corePlugins: {
     preflight: false,
   },
   plugins: [],
-};
+}
