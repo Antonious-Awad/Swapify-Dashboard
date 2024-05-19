15bba4f88860f4fcb9309215023079edb107d129
Antonious-Awad <antonuostony1@gmail.com>
2024-05-19 22:13:50 +0300

    fix: modifying prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,6 +9,13 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ["@typescript-eslint", "react-refresh", "prettier"],
+  extends: [
+        "airbnb",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/typescript",
+        "plugin:storybook/recommended",
+        "airbnb/hooks"
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
