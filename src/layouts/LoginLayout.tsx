6d58a8598f55bea7fcd728f1b3c48839699b6a5b
Antonious-Awad <antonuostony1@gmail.com>
2024-05-19 23:54:56 +0300

    init: layouts

---
+++ src/layouts/LoginLayout.tsx
@@ -0,0 +1,20 @@
+import { Layout } from 'antd'
+import { Outlet } from 'react-router-dom'
+import backgroundImage from '../assets/Images/LoginBackground.png'
+
+export const LoginLayout = () => {
+  return (
+    <Layout>
+      <Layout.Content>
+        <div className="w-full relative h-screen overflow-hidden text-center text-h1SemiBold bg-neutral-200">
+          <img
+            src={backgroundImage}
+            alt="background"
+            className="absolute w-full max-w-full max-h-full top-0 right-0 bottom-[-0.5rem] left-0 overflow-hidden object-cover h-[calc(100%+8px)]"
+          />
+          <Outlet />
+        </div>
+      </Layout.Content>
+    </Layout>
+  )
+}
