9deeaec56bee803ccda05ebe4827e520ca1f7a6e
Antonious-Awad <antonuostony1@gmail.com>
2024-05-24 00:52:03 +0300

    adding token to headers of axios instance

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,16 @@
-import axios from 'axios'
+import axios, { InternalAxiosRequestConfig } from 'axios'
+import { checkValidToken, getSessionsToken } from './token'
 
 const API_URL = 'http://localhost:8000/Admin'
 
 const axiosInstance = axios.create({ baseURL: API_URL })
+
+axiosInstance.interceptors.request.use((config: InternalAxiosRequestConfig) => {
+  const token = checkValidToken() ? getSessionsToken() : ''
+
+  if (token) config.headers.token = token
+
+  return config
+})
 
 export default axiosInstance
