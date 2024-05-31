61dd549c442237083ed8d251e44c80ed493f6388
Antonious-Awad <antonuostony1@gmail.com>
2024-05-31 22:11:19 +0300

    chore: add edit button and avatar image to admin profile

--- src/pages/AdminProfile/AdminProfile.tsx
+++ src/pages/AdminProfile/AdminProfile.tsx
@@ -3,22 +3,49 @@ import { useModal } from '../../hooks'
 import { GetAdminInfoRes, getAdminInfo } from '../../api/admin'
 import { AxiosResponse } from 'axios'
 import { AppErrorResponse } from '../../common/types'
-import { Avatar, Card, Flex, Space, Typography } from 'antd'
-import { EditOutlined, UserOutlined } from '@ant-design/icons'
+import {
+  Avatar,
+  Button,
+  Card,
+  Flex,
+  FloatButton,
+  Space,
+  Typography,
+} from 'antd'
+import {
+  EditOutlined,
+  EyeInvisibleOutlined,
+  EyeOutlined,
+  UserOutlined,
+} from '@ant-design/icons'
+import { useEffect, useState } from 'react'
 
 export const AdminProfile = () => {
   const { activateModal } = useModal()
   const { Title, Text } = Typography
 
-  const { data: adminInfoRes, isLoading: isFetchingAdminInfo } = useQuery<
-    AxiosResponse<GetAdminInfoRes>,
-    AppErrorResponse
-  >({
+  const [canViewPassword, setCanViewPassword] = useState(false)
+
+  const {
+    data: adminInfoRes,
+    isLoading: isFetchingAdminInfo,
+    isError,
+    error,
+  } = useQuery<AxiosResponse<GetAdminInfoRes>, AppErrorResponse>({
     queryKey: ['get-admin-info'],
     queryFn: getAdminInfo,
   })
 
   const adminInfo = adminInfoRes?.data.data
+
+  useEffect(() => {
+    if (isError) {
+      activateModal(
+        'danger',
+        error.response?.data.message || 'Error fetching admin info'
+      )
+    }
+  })
 
   type AdminInfoKeys = keyof GetAdminInfoRes['data']
   const personalInfoKeys: AdminInfoKeys[] = ['username', 'phone', 'location']
@@ -36,22 +63,20 @@ export const AdminProfile = () => {
   return (
     <div className="p-4">
       <div className="flex items-center">
-        <Avatar size={120} icon={<UserOutlined />} />
+        <Avatar size={120} icon={<UserOutlined />} src={adminInfo?.image} />
       </div>
       <Title level={3} className="my-[3rem]">
         Personal Info
       </Title>
+      {/* TODO: Fix Gaps */}
       <div className="grid grid-cols-2 gap-x-[4rem] gap-y-[2rem]">
         {personalInfoKeys.map((field) => {
           return (
-            <Card loading={isFetchingAdminInfo}>
+            <Card loading={isFetchingAdminInfo} className="w-[25rem]">
               <Space size="middle" direction="vertical" className="w-full">
-                <Flex justify="space-between">
-                  <Text className="text-[18px] text-neutral-300">
-                    {adminInfoTitles[field]}
-                  </Text>
-                  <EditOutlined />
-                </Flex>
+                <Text className="text-[18px] text-neutral-300">
+                  {adminInfoTitles[field]}
+                </Text>
                 <Text className="text-[20px] font-semibold">
                   {field === 'location'
                     ? `${adminInfo?.location.governorate}, ${adminInfo?.location.city}`
@@ -69,22 +94,37 @@ export const AdminProfile = () => {
         {signInMethodKeys.map((field) => {
           if (field === 'location') return
           return (
-            <Card loading={isFetchingAdminInfo}>
+            <Card loading={isFetchingAdminInfo} className="w-[25rem]">
               <Space size="middle" direction="vertical" className="w-full">
-                <Flex justify="space-between">
-                  <Text className="text-[18px] text-neutral-300">
-                    {adminInfoTitles[field]}
-                  </Text>
-                  <EditOutlined />
-                </Flex>
-                <Text className="text-[20px] font-semibold">
-                  {field === 'password' ? '*********' : adminInfo?.[field]}
+                <Text className="text-[18px] text-neutral-300">
+                  {adminInfoTitles[field]}
                 </Text>
+                <Flex justify="space-between">
+                  <Text className="text-[20px] font-semibold px-2">
+                    {field === 'password' && !canViewPassword
+                      ? '*********'
+                      : adminInfo?.[field]}
+                  </Text>
+                  {field === 'password' && (
+                    <Button
+                      type="link"
+                      onClick={() => setCanViewPassword(!canViewPassword)}
+                      className="p-0"
+                    >
+                      {canViewPassword ? (
+                        <EyeOutlined />
+                      ) : (
+                        <EyeInvisibleOutlined />
+                      )}
+                    </Button>
+                  )}
+                </Flex>
               </Space>
             </Card>
           )
         })}
       </div>
+      <FloatButton icon={<EditOutlined />} tooltip={'Edit Admin Info'} />
     </div>
   )
 }
